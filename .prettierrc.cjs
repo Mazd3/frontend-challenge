5c88dc9dcd20aed20a7878f281dcda6e228e1a1f
mazde <n.mazde@gmail.com>
2024-01-31 21:26:47 +0500

    add prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  tabWidth: 4,
+  printWidth: 100,
+  jsxSingleQuote: true,
+  arrowParens: "avoid",
+  trailingComma: "none",
+  semi: true,
+  useTabs: false,
+  singleQuote: true,
+};
